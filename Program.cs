67f81b187d9f8334d35c20ff933d6bc65c68a826
Fail <failkozyrev005@gmail.com>
2024-04-24 12:06:31 +0400

    first

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace pr3krasikov
+{
+    internal class Program
+    {
+        static int a, b;
+        static void Main(string[] args)
+        {
+            vvod();
+            string operation = Console.ReadLine();
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"{a + b}") ;
+                    break;
+                case "-":
+                    Console.WriteLine($"{a - b}");
+                    break;
+                case "*":
+                    Console.WriteLine($"{a * b}");
+                    break;
+                case "/":
+                    Console.WriteLine($"{a / b}");
+                    break;
+
+            }
+        }
+        static void vvod()
+        {
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            
+        }
+        
+        
+        
+    }
+}
