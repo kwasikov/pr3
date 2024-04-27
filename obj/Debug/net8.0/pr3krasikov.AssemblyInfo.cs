127a511e0a0e463ab5c2831867478e7edae73558
Fail <failkozyrev005@gmail.com>
2024-04-27 09:50:09 +0400

    plus

--- obj/Debug/net8.0/pr3krasikov.AssemblyInfo.cs
+++ obj/Debug/net8.0/pr3krasikov.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pr3krasikov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f81b187d9f8334d35c20ff933d6bc65c68a826")]
 [assembly: System.Reflection.AssemblyProductAttribute("pr3krasikov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pr3krasikov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
